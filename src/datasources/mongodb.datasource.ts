bb034acfbd983ec36d825783c0e7a36a05043e77
NextLogic DevOps <system.admin@philwebservices.com>
2023-02-20 15:40:19 +0800

    adding data source and repository

---
+++ src/datasources/mongodb.datasource.ts
@@ -0,0 +1,32 @@
+import {inject, lifeCycleObserver, LifeCycleObserver} from '@loopback/core';
+import {juggler} from '@loopback/repository';
+
+const config = {
+  name: 'mongodb',
+  connector: 'mongodb',
+  url: 'mongodb+srv://lanzruiz:kwievCT4igxCg6BG@cluster0.i1at06q.mongodb.net/?retryWrites=true&w=majority',
+  host: 'cluster0.i1at06q.mongodb.net',
+  port: 27017,
+  user: 'lanzruiz',
+  password: 'kwievCT4igxCg6BG',
+  database: 'user',
+  useNewUrlParser: true
+};
+
+// Observe application's life cycle to disconnect the datasource when
+// application is stopped. This allows the application to be shut down
+// gracefully. The `stop()` method is inherited from `juggler.DataSource`.
+// Learn more at https://loopback.io/doc/en/lb4/Life-cycle.html
+@lifeCycleObserver('datasource')
+export class MongodbDataSource extends juggler.DataSource
+  implements LifeCycleObserver {
+  static dataSourceName = 'mongodb';
+  static readonly defaultConfig = config;
+
+  constructor(
+    @inject('datasources.config.mongodb', {optional: true})
+    dsConfig: object = config,
+  ) {
+    super(dsConfig);
+  }
+}
