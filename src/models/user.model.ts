eb55d5b5b6e98214c11ae74e07c9cefbe4c3d532
NextLogic DevOps <system.admin@philwebservices.com>
2023-02-20 13:47:29 +0800

    creating User Model

---
+++ src/models/user.model.ts
@@ -0,0 +1,64 @@
+import {Entity, model, property} from '@loopback/repository';
+
+@model()
+export class User extends Entity {
+  @property({
+    type: 'number',
+    id: true,
+    generated: true,
+  })
+  id?: number;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  firstName: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  middleName: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  lastName: string;
+
+  @property({
+    type: 'number',
+    required: true,
+  })
+  age: number;
+
+  @property({
+    type: 'date',
+    required: true,
+  })
+  birthDate: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  userName: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  password: string;
+
+
+  constructor(data?: Partial<User>) {
+    super(data);
+  }
+}
+
+export interface UserRelations {
+  // describe navigational properties here
+}
+
+export type UserWithRelations = User & UserRelations;
