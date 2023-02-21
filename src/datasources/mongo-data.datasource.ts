f17b49b41e15e4cfda5428fc4c51518353fdac26
NextLogic DevOps <system.admin@philwebservices.com>
2023-02-21 15:05:51 +0800

    fixing mongodb connectivity

--- src/datasources/mongo-data.datasource.ts
+++ src/datasources/mongo-data.datasource.ts
@@ -2,13 +2,13 @@ import {inject, lifeCycleObserver, LifeCycleObserver} from '@loopback/core';
 import {juggler} from '@loopback/repository';
 
 const config = {
-  name: 'mongodb',
+  name: 'MongoData',
   connector: 'mongodb',
-  url: 'mongodb+srv://lanzruiz:kwievCT4igxCg6BG@cluster0.i1at06q.mongodb.net/?retryWrites=true&w=majority',
-  host: 'cluster0.i1at06q.mongodb.net',
+  url: 'mongodb+srv://lanzruiz:bqgJs1ZHFG7pQRzP@cluster0.i1at06q.mongodb.net/test',
+  host: 'ac-s0mwv1o-shard-00-00.i1at06q.mongodb.net',
   port: 27017,
   user: 'lanzruiz',
-  password: 'kwievCT4igxCg6BG',
+  password: 'x7NXYchrPcepbdCh',
   database: 'user',
   useNewUrlParser: true
 };
@@ -18,13 +18,13 @@ const config = {
 // gracefully. The `stop()` method is inherited from `juggler.DataSource`.
 // Learn more at https://loopback.io/doc/en/lb4/Life-cycle.html
 @lifeCycleObserver('datasource')
-export class MongodbDataSource extends juggler.DataSource
+export class MongoDataDataSource extends juggler.DataSource
   implements LifeCycleObserver {
-  static dataSourceName = 'mongodb';
+  static dataSourceName = 'MongoData';
   static readonly defaultConfig = config;
 
   constructor(
-    @inject('datasources.config.mongodb', {optional: true})
+    @inject('datasources.config.MongoData', {optional: true})
     dsConfig: object = config,
   ) {
     super(dsConfig);
